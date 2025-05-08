eb24434eb7a75e0ffe68ed1e865754c9afd91877
Matti Nordström <mattinordstrom@proton.me>
2025-05-08 13:08:08 +0200

    Message from file

--- main.py
+++ main.py
@@ -3,12 +3,20 @@ from websockets.asyncio.client import connect
 
 port=9097
 
+message = "Hello world!"
+file="example" # Leave empty to use the default message above
+
 async def send_message():
+    global message
     uri = "ws://localhost:" + str(port) + "/moxywsmock"
 
+    if file:
+        with open("files/"+file, "r", encoding="utf-8") as f:
+            message = f.read().strip()
+
     async with connect(uri) as websocket:
-        message = "Hello world!"
         await websocket.send(message)
         print(f"> Sent: {message}")
+
 
 asyncio.run(send_message())
